4f7e5a48e053338bfca2901e992d4554def8547b
Manuel Lorenzo <mlorenzofr@redhat.com>
2025-11-16 12:47:35 +0100

    mbp-1005: Implement multi-role support for Vault JWT (#69)
    Signed-off-by: Manuel Lorenzo <mlorenzofr@redhat.com>

--- common/scripts/vault-utils.sh
+++ common/scripts/vault-utils.sh
@@ -21,7 +21,7 @@ fi
 TASK="${1}"
 PATTERN_NAME=${2:-$(basename "`pwd`")}
 
-if [ -z ${TASK} ]; then
+if [ -z "${TASK}" ]; then
 	echo "Task is unset"
 	exit 1
 fi
@@ -31,17 +31,13 @@ EXTRA_PLAYBOOK_OPTS="${EXTRA_PLAYBOOK_OPTS:-}"
 if [ "$(yq ".clusterGroup.applications.vault.jwt.enabled // \"false\"" "${MAIN_CLUSTERGROUP_FILE}")" == "true" ]; then
   OCP_DOMAIN="$(oc get dns cluster -o jsonpath='{.spec.baseDomain}')"
   OIDC_DISCOVERY_URL="$(yq ".clusterGroup.applications.vault.jwt.oidcDiscoveryUrl" "${MAIN_CLUSTERGROUP_FILE}" | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
-  DEFAULT_ROLE="$(yq ".clusterGroup.applications.vault.jwt.roles[0].name" "${MAIN_CLUSTERGROUP_FILE}")"
-  SPIFFE_AUDIENCE="$(yq ".clusterGroup.applications.vault.jwt.roles[0].audience" "${MAIN_CLUSTERGROUP_FILE}")"
-  SPIFFE_SUBJECT="$(yq ".clusterGroup.applications.vault.jwt.roles[0].subject" "${MAIN_CLUSTERGROUP_FILE}" | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
+  JWT_ROLES="$(yq -o json ".clusterGroup.applications.vault.jwt.roles" "${MAIN_CLUSTERGROUP_FILE}" | jq -rc | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
 
-  if [ "${OIDC_DISCOVERY_URL}" == "null" ] || [ "${DEFAULT_ROLE}" == "null" ] || [ "${SPIFFE_AUDIENCE}" == "null" ] || [ "${SPIFFE_SUBJECT}" == "null" ]; then
+  if [ "${OIDC_DISCOVERY_URL}" == "null" ] || [ "${JWT_ROLES}" == "null" ] || [ "${JWT_ROLES}" == "[]" ]; then
     echo "Vault JWT config is disabled because of missing required fields"
     VAULT_JWT_CONFIG="false"
     echo "OIDC_DISCOVERY_URL: ${OIDC_DISCOVERY_URL}"
-    echo "DEFAULT_ROLE: ${DEFAULT_ROLE}"
-    echo "SPIFFE_AUDIENCE: ${SPIFFE_AUDIENCE}"
-    echo "SPIFFE_SUBJECT: ${SPIFFE_SUBJECT}"
+    echo "JWT_ROLES: ${JWT_ROLES}"
     echo "Vault JWT config is disabled"
   else
     VAULT_JWT_CONFIG="true"
@@ -58,7 +54,5 @@ ansible-playbook -t "${TASK}" \
   -e pattern_dir="${PATTERNPATH}" \
   -e vault_jwt_config="${VAULT_JWT_CONFIG}" \
   -e oidc_discovery_url="${OIDC_DISCOVERY_URL:-}" \
-  -e default_role="${DEFAULT_ROLE:-}" \
-  -e spiffe_audience="${SPIFFE_AUDIENCE:-}" \
-  -e spiffe_subject="${SPIFFE_SUBJECT:-}" \
+  -e vault_jwt_roles="${JWT_ROLES:-'[]'}" \
   ${EXTRA_PLAYBOOK_OPTS} "rhvp.cluster_utils.vault"
